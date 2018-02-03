c170bc77089689d8278e7a78b8959e46a1531d77
nam <Nothingis>
2018-02-03 15:07:26 +0900

    20180203

---
+++ chapter03/ex3_43.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+	int ia[3][4] = 
+	{
+		{ 0, 1, 2, 3 },
+		{ 4, 5, 6, 7 },
+		{ 8, 9, 10, 11}
+	};
+
+	for (int(&a)[4] : ia)
+		for (int b : a) std::cout << b << " ";
+	std::cout << std::endl;
+
+
+	for (size_t i = 0; i != 3; ++i)
+		for (size_t j = 0; j != 4; ++j) std::cout << ia[i][j] << " ";
+	std::cout << std::endl;
+
+
+	for (int(*row)[4] = ia; row != ia + 3; ++row)
+		for (int *col = *row; col != *row + 4; ++col)
+			std::cout << *col << " ";
+	std::cout << std::endl;
+
+	return 0;
+}
---
+++ chapter03/ex3_44.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main()
+{
+	int ia[3][4] = 
+	{
+		{ 0, 1, 2, 3 },
+		{ 4, 5, 6, 7 },
+		{ 8, 9, 10, 11}
+	};
+
+	using row = int[4];
+	for (row& a : ia)
+		for (int b : a) std::cout << b << " ";
+	std::cout << std::endl;
+
+
+	for (size_t i = 0; i != 3; ++i)
+		for (size_t j = 0; j != 4; ++j) std::cout << ia[i][j] << " ";
+	std::cout << std::endl;
+
+
+	for (row* a = ia; a != ia + 3; ++a)
+		for (int *col = *a; col != *a + 4; ++col)
+			std::cout << *col << " ";
+	std::cout << std::endl;
+
+	return 0;
+}
